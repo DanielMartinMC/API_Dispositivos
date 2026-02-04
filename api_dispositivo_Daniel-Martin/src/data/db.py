167fb8b27ccff65657463e46ff7bb2ae1b37a54b
Daniel Martin <daniel.martin48@educa.madrid.org>
2026-02-04 17:27:33 +0100

    hola

--- api_dispositivo_Daniel-Martin/src/data/db.py
+++ api_dispositivo_Daniel-Martin/src/data/db.py
@@ -13,6 +13,7 @@ if not DATABASE_URL:
     db_port: int = os.getenv("DB_PORT", 5432)  
     db_name: str = os.getenv("DB_NAME", "dispositivos_db")
     DATABASE_URL = f"postgresql+psycopg2://{db_user}:{db_password}@{db_server}:{db_port}/{db_name}"
+    
 else:
     print("Using provided DATABASE_URL")
     
